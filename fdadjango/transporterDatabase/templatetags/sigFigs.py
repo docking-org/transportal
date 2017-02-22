e9eb8656b9055a03f5c38a062495680fedfe995b
Teague Sterling <teaguesterling@gmail.com>
2017-02-21 21:48:33 -0800

    Initial commit

---
+++ fdadjango/transporterDatabase/templatetags/sigFigs.py
@@ -0,0 +1,46 @@
+from django import template
+
+register = template.Library()
+def showSigFigs(d, args):
+	args = args.split(',')
+	sigFigsLeft = int(args[0])
+	numFrontDigits = int(args[1])
+	front = ''
+	for x in range(numFrontDigits-1,0,-1 ):
+		if int(d)/(10**x) > 0:
+			break
+		front = front+'&nbsp;'
+	if int(d)/1 == 0:
+		temp = d
+		while True:
+			temp = 10 * temp
+			if int(temp)/1 > 0:
+				break
+			sigFigsLeft += 1
+	if sigFigsLeft < 0:
+		sigFigsLeft = 0
+	return front + '{0:.{1}f}'.format(d,sigFigsLeft) 
+register.filter('showSigFigs', showSigFigs)
+def showSigFigs1(d, args):
+        args = args.split(',')
+        sigFigsLeft = int(args[0])
+        numFrontDigits = int(args[1])
+        front = ''
+        for x in range(numFrontDigits-1,0,-1 ):
+                if int(d)/(10**x) > 0:
+                        sigFigsLeft = sigFigsLeft - x - 1
+                        break
+                front = front+'&nbsp;'
+        if int(d)/1 > 0 and int(d)/10 == 0:
+		sigFigsLeft = sigFigsLeft - 1
+        if int(d)/1 == 0:
+                temp = d
+                while True:
+                        temp = 10 * temp
+                        if int(temp)/1 > 0:
+                                break   
+                        sigFigsLeft += 1
+        if sigFigsLeft < 0:
+                sigFigsLeft = 0
+        return front + '{0:.{1}f}'.format(d,sigFigsLeft)
+register.filter('showSigFigs1', showSigFigs1)
