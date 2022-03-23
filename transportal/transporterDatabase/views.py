977c5c07820d6b1339255f19561b2ea1d9e69d31
Christopher Wen <ccwen6@gmail.com>
2022-03-22 21:47:40 -0700

    Update views.py

--- transportal/transporterDatabase/views.py
+++ transportal/transporterDatabase/views.py
@@ -79,13 +79,13 @@ def transporter(request, transporter_id):
                 cmpndList[x.cmpnd] = ''
                 cmpndList[x.substrate] = ''
         for cmpnd in cmpndList:
-                if cmpnd.slugName in transporter.inVitroSubstrate:
+                if cmpnd in transporter.inVitroSubstrate.all():
                         cmpndList[cmpnd] += '1'
-                if cmpnd.slugName in transporter.inVitroInhibitor:
+                if cmpnd in transporter.inVitroInhibitor.all():
                         cmpndList[cmpnd] += '2'
-                if cmpnd.slugName in transporter.clinSubstrate:
+                if cmpnd in transporter.clinSubstrate.all():
                         cmpndList[cmpnd] += '3'
-                if cmpnd.slugName in transporter.clinInhibitor:
+                if cmpnd in transporter.clinInhibitor.all():
                         cmpndList[cmpnd] += '4'
         return render_to_response('transporter.html', {'expression': buildExp, 'transporter':transporter, 'important': importantNames, 'substrates': substrates, 'inhibitors':inhibitors, 'ddi':ddiInfo, 'otherTrans':otherTrans, 'fdaCmpnds':cmpndList})
 
